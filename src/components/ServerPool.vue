927b0cade3081f143f9dd7c3c1cc48afb297de6f
shreyesbenak <shreyesbenak@gmail.com>
2025-01-15 12:33:11 +0530

    first with benchMark file

---
+++ src/components/ServerPool.vue
@@ -0,0 +1,13 @@
+<!-- ServerPool.vue -->
+<template>
+    <div>
+      <h1>Server Pool Component</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ServerPool',
+  };
+  </script>
+  
